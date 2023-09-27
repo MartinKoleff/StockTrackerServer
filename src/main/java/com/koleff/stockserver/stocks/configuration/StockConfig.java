5aefa716c8f781ad9d4a3ec63708cf982a7ffb9a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-09-27 15:15:00 +0300

    Added Stock Configuration

---
+++ src/main/java/com/koleff/stockserver/stocks/configuration/StockConfig.java
@@ -0,0 +1,36 @@
+package com.koleff.stockserver.stocks.configuration;
+
+import com.koleff.stockserver.stocks.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class StockConfig {
+
+    @Value("${app.useFakeStudentRepository:false}")
+    private Boolean useFakeStudentRepository;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired //inject in constructor...
+    private Environment environment;
+
+    @Bean
+    CommandLineRunner commandLineRunner(InfoApp infoApp) {
+        return args -> {
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
+            System.out.printf("%s | %s | %s\n",
+                    infoApp.getName(),
+                    infoApp.getDescription(),
+                    infoApp.getVersion()
+            );
+        };
+    }
+}
+
