8f14572cdb35e1b8678cbde174f55308f6efebca
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-09-27 15:06:59 +0300

    Added Test class.

---
+++ src/test/java/com/koleff/stockserver/StockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koleff.stockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
