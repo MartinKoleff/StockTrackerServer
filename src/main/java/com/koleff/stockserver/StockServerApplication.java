96b8bd6960d105c9036663249fb44a330c22489c
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-09-27 22:58:57 +0300

    Added configuration tags for app class.

--- src/main/java/com/koleff/stockserver/StockServerApplication.java
+++ src/main/java/com/koleff/stockserver/StockServerApplication.java
@@ -3,10 +3,13 @@ package com.koleff.stockserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaRepositories
+@EnableConfigurationProperties
 public class StockServerApplication {
 
     public static void main(String[] args) {
