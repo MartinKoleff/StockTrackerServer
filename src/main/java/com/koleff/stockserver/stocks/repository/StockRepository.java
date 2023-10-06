038ab41bdeaf3cccf0c41c013b2f5b7d227ff19d
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-06 23:02:09 +0300

    Replaced old repository with new. Added example queries for future.

--- src/main/java/com/koleff/stockserver/stocks/repository/StockRepository.java
+++ src/main/java/com/koleff/stockserver/stocks/repository/StockRepository.java
@@ -2,9 +2,38 @@ package com.koleff.stockserver.stocks.repository;
 
 import com.koleff.stockserver.stocks.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+    @Query("SELECT s FROM Stock s WHERE s.tag = ?1")
+    Optional<Stock> findStockByStockTag(String stockTag);
+
+    @Query(
+            value = "SELECT * FROM stock WHERE has_intra_day = :hasIntraDay",
+            nativeQuery = true)
+    List<Stock> selectStockWhereHasIntraDayEqualTrue(
+            @Param("hasIntraDay") Boolean hasIntraDay
+    );
+
+    @Query(
+            value = "SELECT * FROM stock WHERE has_end_of_day = :hasEndOfDay",
+            nativeQuery = true)
+    List<Stock> selectStockWhereHasEndOfDayEqualTrue(
+            @Param("hasEndOfDay") Boolean hasEndOfDay
+    );
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Stock s WHERE s.id = ?1")
+    int deleteStockById(Long id);
 }
