dfbe050587870757b0533abe288796953d742153
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-09 23:33:15 +0300

    Added IntraDay Controller.

---
+++ src/main/java/com/koleff/stockserver/stocks/controller/IntraDayController.java
@@ -0,0 +1,18 @@
+package com.koleff.stockserver.stocks.controller;
+
+import com.koleff.stockserver.stocks.dto.StockDto;
+import com.koleff.stockserver.stocks.service.IntraDayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/intraday/")
+public class IntraDayController {
+
+    private final IntraDayService intraDayService;
+
+    @Autowired
+    public IntraDayController(IntraDayService intraDayService) {
+        this.intraDayService = intraDayService;
+    }
+}
