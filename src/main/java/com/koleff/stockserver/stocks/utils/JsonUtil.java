8f2f248641321a4c0b412f6aa78d994577e62527
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-10 16:41:48 +0300

    Refactoring JsonUtil. Added comments.

--- src/main/java/com/koleff/stockserver/stocks/utils/JsonUtil.java
+++ src/main/java/com/koleff/stockserver/stocks/utils/JsonUtil.java
@@ -1,25 +1,38 @@
 package com.koleff.stockserver.stocks.utils;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.gson.Gson;
+import com.koleff.stockserver.stocks.domain.IntraDay;
+import com.koleff.stockserver.stocks.domain.wrapper.DataWrapper;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 public class JsonUtil<T> {
 
-    private final Class<T> type;
+    private Type type;
+    private final String resourcePath = "src/main/resources/json/%s";
 
     //Generics are created on build time. They are lost on run time.
-    public JsonUtil(Class<T> type) {
+    public void setType(Type type) {
         this.type = type;
     }
 
-    public String getJson(String jsonPath) {
-        String filePath = String.format("src/main/resources/json/%s", jsonPath);
+    /**
+     * Load JSON from file
+     */
+    public String loadJson(String jsonPath) {
+        String filePath = String.format(resourcePath, jsonPath);
         File jsonFile = new File(filePath);
 
         String json = null;
@@ -30,7 +43,7 @@ public class JsonUtil<T> {
                     new FileReader(jsonFile)
             );
 
-             json = data.toJSONString();
+            json = data.toJSONString();
         } catch (IOException | ParseException e) {
             System.out.println("Failed to parse JSON.");
         }
@@ -38,7 +51,10 @@ public class JsonUtil<T> {
         return json;
     }
 
-    public T convertJson(String json){
+    /**
+     * Map JSON to custom object
+     */
+    public T convertJson(String json) {
         Gson gson = new Gson();
         return gson.fromJson(json, type);
     }
