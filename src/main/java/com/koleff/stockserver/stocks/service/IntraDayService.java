98bfff4718bce28d2099583abaecedad274c91a2
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-12 23:37:16 +0300

    Expanded and fully refactored IntraDay Controller. Added validation.
    Separated loading from JSON functionality, saving functionality to DB and fetch functionality from DB.

--- src/main/java/com/koleff/stockserver/stocks/service/IntraDayService.java
+++ src/main/java/com/koleff/stockserver/stocks/service/IntraDayService.java
@@ -13,12 +13,15 @@ import com.koleff.stockserver.stocks.utils.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class IntraDayService {
     private final IntraDayRepository intraDayRepository;
     private final StockRepository stockRepository;
+    private final StockService stockService;
+
     private final IntraDayDtoMapper intraDayDtoMapper;
     private final JsonUtil<DataWrapper<IntraDay>> jsonUtil;
 
@@ -26,10 +29,12 @@ public class IntraDayService {
     public IntraDayService(IntraDayRepository intraDayRepository,
                            IntraDayDtoMapper intraDayDtoMapper,
                            StockRepository stockRepository,
+                           StockService stockService,
                            JsonUtil<DataWrapper<IntraDay>> jsonUtil) {
         this.intraDayRepository = intraDayRepository;
         this.intraDayDtoMapper = intraDayDtoMapper;
         this.stockRepository = stockRepository;
+        this.stockService = stockService;
         this.jsonUtil = jsonUtil;
     }
 
@@ -61,38 +66,108 @@ public class IntraDayService {
                 .toList();
     }
 
-    /**REFACTOR TO NOT TAKE FROM JSON...*/
-    public void saveIntraDay() {
+    //List for all stocks and inside each list -> list of their intraday
+    public void saveAllIntraDay(List<List<IntraDay>> data) {
         stockRepository.findAll().stream()
                 .map(Stock::getTag)
                 .forEach(stockTag -> {
-                    //Configure json based on current stock
-                    String filePath = String.format("intraday%s.json", stockTag);
-
-                    //Load data from json
-                    jsonUtil.setType(IntraDay.class);
-                    String json = jsonUtil.loadJson(filePath);
-                    DataWrapper<IntraDay> data = jsonUtil.convertJson(json);
 
                     //Configure stock_id
-                    data.getData()
-                            .forEach(intraDay -> {
-                                intraDay.setStockId(
-                                        stockRepository.findStockByStockTag(stockTag)
-                                                .orElseThrow(
-                                                        () -> new IntraDayNotSavedException(
-                                                                String.format(
-                                                                        "Intra day for stock %s could not be saved.",
-                                                                        stockTag
-                                                                )
-                                                        )
-                                                )
-                                                .getId()
-                                );
-                            });
+                    data.forEach(intraDay ->
+                            intraDay.forEach(entry ->
+                                    entry.setStockId(
+                                            stockRepository.findStockByStockTag(stockTag)
+                                                    .orElseThrow(
+                                                            () -> new IntraDayNotSavedException(
+                                                                    String.format(
+                                                                            "Intra day for stock %s could not be saved.",
+                                                                            stockTag
+                                                                    )
+                                                            )
+                                                    )
+                                                    .getId()
+                                    )
+                            ));
+
                     //Save data entities to DB
-                    intraDayRepository.saveAll(data.getData());
+                    data.forEach(intraDayRepository::saveAll);
                 });
+    }
+
+    public void saveIntraDay(List<IntraDay> data) {
+        stockRepository.findAll().stream()
+                .map(Stock::getTag)
+                .forEach(stockTag -> {
+
+                    //Configure stock_id
+                    data.forEach(entry ->
+                            entry.setStockId(
+                                    stockRepository.findStockByStockTag(stockTag)
+                                            .orElseThrow(
+                                                    () -> new IntraDayNotSavedException(
+                                                            String.format(
+                                                                    "Intra day for stock %s could not be saved.",
+                                                                    stockTag
+                                                            )
+                                                    )
+                                            )
+                                            .getId()
+                            )
+                    );
+
+                    //Save data entities to DB
+                    intraDayRepository.saveAll(data);
+                });
+    }
+
+    public List<IntraDay> loadIntraDay(String stockTag) {
+        //Configure json based on current stock
+        String filePath = String.format("intraday%s.json", stockTag);
+
+        //Load data from json
+        jsonUtil.setType(IntraDay.class);
+        String json = jsonUtil.loadJson(filePath);
+        DataWrapper<IntraDay> data = jsonUtil.convertJson(json);
+
+        return data.getData();
+    }
+
+    public List<List<IntraDay>> loadAllIntraDays() {
+        List<List<IntraDay>> data = new ArrayList<>();
+
+        List<String> stockTags = stockService.getStockTags();
+        stockTags.forEach(
+                stockTag -> {
+                    List<IntraDay> entry = loadIntraDay(stockTag);
+                    data.add(entry);
+                }
+        );
+
+        return data;
+    }
+
+    public List<List<IntraDayDto>> getAllIntraDays() {
+        List<List<IntraDayDto>> data = new ArrayList<>();
+
+        List<String> stockTags = stockService.getStockTags();
+        stockTags.forEach(
+                stockTag -> {
+                    List<IntraDayDto> entry = intraDayRepository.findIntraDayByStockTag(stockTag)
+                            .orElseThrow(
+                                    () -> new IntraDayNotFoundException(
+                                            String.format("Intra day for stock tag %s not found.",
+                                                    stockTag
+                                            )
+                                    )
+                            )
+                            .stream()
+                            .map(intraDayDtoMapper)
+                            .toList();
+                    data.add(entry);
+                }
+        );
+
+        return data;
     }
 }
 
