a1ec234b19287fdac201173e6d6fee4a8f44f5f4
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-12 23:36:34 +0300

    Fully refactored Stock Controller. Added parameter validation.
    Separated loading from JSON functionality, saving functionality to DB and fetch functionality from DB.

--- src/main/java/com/koleff/stockserver/stocks/controller/StockController.java
+++ src/main/java/com/koleff/stockserver/stocks/controller/StockController.java
@@ -1,7 +1,9 @@
 package com.koleff.stockserver.stocks.controller;
 
+import com.koleff.stockserver.stocks.domain.Stock;
 import com.koleff.stockserver.stocks.dto.StockDto;
 import com.koleff.stockserver.stocks.service.StockService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,23 +20,50 @@ public class StockController {
         this.stockService = stockService;
     }
 
-    @GetMapping(path = "stocks")
+    @GetMapping(path = "get/all")
     public List<StockDto> getStocks() {
         return stockService.getStocks();
     }
 
-    @GetMapping(path = "{stockId}")
-    public StockDto getStock(@PathVariable("stockId") Long id) {
+    @GetMapping(path = "get/{stock_id}")
+    public StockDto getStock(@PathVariable("stock_id") Long id) {
         return stockService.getStock(id);
     }
 
-    @PutMapping(path = "save/{stockTag}")
-    public void saveStock(@PathVariable("stockTag") String stockTag){
-        stockService.saveStock(stockTag);
+    @GetMapping(path = "get/{stock_tag}")
+    public StockDto getStock(@PathVariable("stock_tag") String stockTag) {
+        return stockService.getStock(stockTag);
     }
 
+    /**
+     * Save bulk to DB
+     */
     @PutMapping(path = "save/all")
-    public void saveStocks(){
-        stockService.saveStocks();
+    public void saveStocks(@Valid List<Stock> stocks) {
+        stockService.saveStocks(stocks);
+    }
+
+    /**
+     * Save to DB
+     */
+    @PutMapping(path = "save")
+    public void saveStock(@Valid Stock stock) {
+        stockService.saveStock(stock);
+    }
+
+    /**
+     * Load all from JSON
+     */
+    @GetMapping(path = "load/all")
+    public List<Stock> loadStocks() {
+        return stockService.loadStocks();
+    }
+
+    /**
+     * Load from JSON
+     */
+    @PutMapping(path = "load/{stockTag}")
+    public Stock loadStock(@PathVariable("stockTag") String stockTag) {
+        return stockService.loadStock(stockTag);
     }
 }
