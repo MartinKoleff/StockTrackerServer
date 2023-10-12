2dcbab01976a8e612bef729f15ce056eeca45217
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-12 21:28:34 +0300

    Added OpenFeign dependency.

--- src/main/java/com/koleff/stockserver/StockServerApplication.java
+++ src/main/java/com/koleff/stockserver/StockServerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaRepositories
+@EnableFeignClients
 @EnableConfigurationProperties
 public class StockServerApplication {
 
