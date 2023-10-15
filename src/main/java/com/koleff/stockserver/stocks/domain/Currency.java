a5d6ca2aaacd49e24123e986afb43f13fc630518
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-16 00:19:02 +0300

    Added 2 new tables to expand StockExchange.

---
+++ src/main/java/com/koleff/stockserver/stocks/domain/Currency.java
@@ -0,0 +1,69 @@
+package com.koleff.stockserver.stocks.domain;
+
+import com.google.gson.annotations.SerializedName;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity(name = "Currency")
+@Table(name = "currency")
+@NoArgsConstructor
+@AllArgsConstructor
+public@Data class Currency implements Serializable {
+    @Id
+    @SequenceGenerator(
+            name = "currency_sequence",
+            sequenceName = "currency_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "currency_sequence"
+    )
+    @Column(
+            name = "id"
+    )
+    @SerializedName("id")
+    private Long id;
+
+    @Column(
+            name = "code",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotNull(message = "Code must not be empty.")
+    @SerializedName("code")
+    private String code;
+
+    @Column(
+            name = "symbol",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotNull(message = "Symbol must not be empty.")
+    @SerializedName("symbol")
+    private String symbol;
+
+    @Column(
+            name = "name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotNull(message = "Name must not be empty.")
+    @SerializedName("name")
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "id=" + id +
+                ", code='" + code + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/koleff/stockserver/stocks/domain/Timezone.java
@@ -0,0 +1,69 @@
+package com.koleff.stockserver.stocks.domain;
+
+import com.google.gson.annotations.SerializedName;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity(name = "Timezone")
+@Table(name = "timezone")
+@NoArgsConstructor
+@AllArgsConstructor
+public@Data class Timezone implements Serializable {
+    @Id
+    @SequenceGenerator(
+            name = "timezone_sequence",
+            sequenceName = "timezone_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "timezone_sequence"
+    )
+    @Column(
+            name = "id"
+    )
+    @SerializedName("id")
+    private Long id;
+
+    @Column(
+            name = "timezone",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotNull(message = "Timezone must not be empty.")
+    @SerializedName("timezone")
+    private String timezone;
+
+    @Column(
+            name = "abbreviation",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotNull(message = "Abbreviation must not be empty.")
+    @SerializedName("abbreviation")
+    private String abbreviation; //Timezone abbreviation
+
+    @Column(
+            name = "abbreviation_dst",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    @NotNull(message = "Abbreviation daylight-saving time must not be empty.")
+    @SerializedName("abbreviation_dst")
+    private String abbreviationDst; //Summer time timezone abbreviation (DST -> daylight-saving time)
+
+    @Override
+    public String toString() {
+        return "Timezone{" +
+                "id=" + id +
+                ", timezone='" + timezone + '\'' +
+                ", abbreviation='" + abbreviation + '\'' +
+                ", abbreviationDst='" + abbreviationDst + '\'' +
+                '}';
+    }
+}
