6288c50f942f22a01976e1ce06fd07f5fd0e5e43
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-16 17:56:41 +0300

    Added more queries to IntraDayRepository.

--- src/main/java/com/koleff/stockserver/stocks/repository/IntraDayRepository.java
+++ src/main/java/com/koleff/stockserver/stocks/repository/IntraDayRepository.java
@@ -1,15 +1,17 @@
 package com.koleff.stockserver.stocks.repository;
 
 import com.koleff.stockserver.stocks.domain.IntraDay;
-import com.koleff.stockserver.stocks.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional(readOnly = true)
 public interface IntraDayRepository extends JpaRepository<IntraDay, Long> {
 
     @Query(
@@ -24,5 +26,14 @@ public interface IntraDayRepository extends JpaRepository<IntraDay, Long> {
                     "WHERE id.id = 1?",
             nativeQuery = true)
     Optional<List<IntraDay>> findAllById(Long stockId);
+
+    @Transactional
+    @Modifying
+    @Query(
+            value = "DELETE FROM intra_day id " +
+                    "JOIN stock s ON (s.id = id.stock_id) " +
+                    "WHERE s.tag = 1?",
+            nativeQuery = true)
+    int deleteByStockTag(String stockTag);
 }
 
