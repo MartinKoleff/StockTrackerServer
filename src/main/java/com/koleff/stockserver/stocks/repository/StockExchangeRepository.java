df0de131219cfe965c1f016f40c56609a2b037b8
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-16 17:56:52 +0300

    Added more queries to StockExchangeRepository..

--- src/main/java/com/koleff/stockserver/stocks/repository/StockExchangeRepository.java
+++ src/main/java/com/koleff/stockserver/stocks/repository/StockExchangeRepository.java
@@ -1,9 +1,18 @@
 package com.koleff.stockserver.stocks.repository;
 
 import com.koleff.stockserver.stocks.domain.StockExchange;
+import com.koleff.stockserver.stocks.dto.StockExchangeDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StockExchangeRepository extends JpaRepository<StockExchange, Long> {
+
+    @Query(
+            value = "SELECT * FROM stockExchange se WHERE se.country = ?1",
+            nativeQuery = true)
+    List<StockExchange> getStockExchangeByCountry(String country);
 }
