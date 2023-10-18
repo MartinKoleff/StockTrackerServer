47892a20401b156183a604c2f33b85ac4f462506
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-18 17:14:59 +0300

    Fixed server error by adding getMapping to base OpenFeign class.
    Removed FeignClient from base class. Only to be inherited from.

--- src/main/java/com/koleff/stockserver/remoteApi/client/v2/base/PublicApiClientV2.java
+++ src/main/java/com/koleff/stockserver/remoteApi/client/v2/base/PublicApiClientV2.java
@@ -1,18 +1,12 @@
 package com.koleff.stockserver.remoteApi.client.v2.base;
 
 import com.koleff.stockserver.stocks.domain.wrapper.DataWrapper;
-import com.koleff.stockserver.stocks.dto.validation.DatabaseTableDto;
-import jakarta.validation.Valid;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(
-        value = "publicApiClient", //Used for dependency injection
-        url = "${spring.cloud.openfeign.client.config.postClient.url}"
-)
 public interface PublicApiClientV2<T> {
+
+    @GetMapping("get")
     DataWrapper<T> getData(@RequestParam String apiKey,
                            @RequestParam String stockTag);
 }
