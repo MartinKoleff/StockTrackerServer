6d63d84a3106a7f2754677ad6efd026fb266b7f7
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-10-23 17:24:10 +0300

    Added base repositories to TestConfig.

--- src/test/java/com/koleff/stockserver/TestConfiguration.java
+++ src/test/java/com/koleff/stockserver/TestConfiguration.java
@@ -1,11 +1,18 @@
 package com.koleff.stockserver;
 
 import com.koleff.stockserver.stocks.repository.*;
+import com.koleff.stockserver.stocks.repository.impl.*;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(basePackageClasses = {
+        StockRepositoryImpl.class,
+        IntraDayRepositoryImpl.class,
+        EndOfDayRepositoryImpl.class,
+        StockExchangeRepositoryImpl.class,
+        CurrencyRepositoryImpl.class,
+        TimezoneRepositoryImpl.class,
         StockRepository.class,
         IntraDayRepository.class,
         EndOfDayRepository.class,
