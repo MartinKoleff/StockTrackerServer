8cbf7c23037becdac6112628146d5b036eae388d
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-01 17:58:52 +0200

    Added date filtration endpoint for EOD in controller.

--- src/main/java/com/koleff/stockserver/stocks/controller/EndOfDayController.java
+++ src/main/java/com/koleff/stockserver/stocks/controller/EndOfDayController.java
@@ -26,14 +26,25 @@ public class EndOfDayController {
     }
 
     @GetMapping("get/{stock_id}")
-    public List<EndOfDayDto> getEndOfDay(@PathVariable("stock_id") Long stockId) {
-        return endOfDayServiceImpl.getEndOfDay(stockId);
+    public List<EndOfDayDto> getEndOfDays(@PathVariable("stock_id") Long stockId) {
+        return endOfDayServiceImpl.getEndOfDays(stockId);
     }
 
     @GetMapping("get/{stock_tag}")
-    public List<EndOfDayDto> getEndOfDay(@PathVariable("stock_tag") String stockTag) {
-        return endOfDayServiceImpl.getEndOfDay(stockTag);
+    public List<EndOfDayDto> getEndOfDays(@PathVariable("stock_tag") String stockTag) {
+        return endOfDayServiceImpl.getEndOfDays(stockTag);
     }
+
+    @GetMapping("get/{stock_tag}")
+    public List<EndOfDayDto> getEndOfDays(@PathVariable("stock_tag") String stockTag, String dateFrom, String dateTo) {
+        return endOfDayServiceImpl.getEndOfDays(stockTag, dateFrom, dateTo);
+    }
+
+    @GetMapping("get/{stock_tag}")
+    public EndOfDayDto getEndOfDay(@PathVariable("stock_tag") String stockTag, String date) {
+        return endOfDayServiceImpl.getEndOfDay(stockTag, date);
+    }
+
 
     /**
      * Save bulk to DB
