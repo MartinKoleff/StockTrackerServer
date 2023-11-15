f2e8aa06ecc9013a719f35fcbcede677732bfb1e
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-15 13:03:03 +0200

    Added error handler for all types of errors.

--- src/main/java/com/koleff/stockserver/stocks/exceptions/handler/ApiExceptionHandler.java
+++ src/main/java/com/koleff/stockserver/stocks/exceptions/handler/ApiExceptionHandler.java
@@ -1,8 +1,7 @@
 package com.koleff.stockserver.stocks.exceptions.handler;
 
-import com.koleff.stockserver.stocks.exceptions.ApiException;
-import com.koleff.stockserver.stocks.exceptions.ApiRequestException;
-import com.koleff.stockserver.stocks.exceptions.StockNotFoundException;
+import com.koleff.stockserver.stocks.exceptions.*;
+import com.koleff.stockserver.stocks.utils.dateUtil.DateUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
@@ -10,46 +9,122 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
     private final static Logger logger = LogManager.getLogger(ApiExceptionHandler.class);
-    @ExceptionHandler(value = ApiRequestException.class) //To make unified error handler. All ApiRequestExceptions to have the payload data inside them (integrate ApiException inside ApiRequestException).
-    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
+
+    @ExceptionHandler(value = ApiRequestException.class)
+    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
 
         //Returned to the client
         ApiException payload = new ApiException(
                 e.getMessage(),
                 e.getCause(),
                 HttpStatus.BAD_REQUEST,
-                ZonedDateTime.now()
+                DateUtil.toISO8601(LocalDateTime.now())
         );
 
         logger.error("Error thrown with status code: " + payload.getHttpStatusCode());
 
-        return new ResponseEntity(
+        return new ResponseEntity<>(
                 payload,
                 payload.getHttpStatus()
         );
     }
 
-    @ExceptionHandler(value = StockNotFoundException.class)
-    public ResponseEntity<Object> handleStockNotFoundException(StockNotFoundException e){
+    @ExceptionHandler(value = {
+            CurrenciesNotFoundException.class,
+            CurrencyNotFoundException.class,
+            EndOfDayNotFoundException.class,
+            IntraDayNotFoundException.class,
+            StockExchangeNotFoundException.class,
+            StockNotFoundException.class,
+            StocksNotFoundException.class,
+            TimezoneNotFoundException.class,
+            TimezonesNotFoundException.class
+    })
+    public ResponseEntity<Object> handleNotFoundException(RuntimeException e) {
 
         //Returned to the client
         ApiException payload = new ApiException(
                 e.getMessage(),
                 e.getCause(),
                 HttpStatus.NOT_FOUND,
-                ZonedDateTime.now()
+                DateUtil.toISO8601(LocalDateTime.now())
         );
 
         logger.error("Error thrown with status code: " + payload.getHttpStatusCode());
 
-        return new ResponseEntity(
+        return new ResponseEntity<>(
                 payload,
                 payload.getHttpStatus()
         );
     }
+
+    @ExceptionHandler(value = {
+            DataNotSavedException.class,
+            IntraDayNotSavedException.class,
+            //TODO: add for each entity...
+    })
+    public ResponseEntity<Object> handleNotSavedException(RuntimeException e) {
+
+        //Returned to the client
+        ApiException payload = new ApiException(
+                e.getMessage(),
+                e.getCause(),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                DateUtil.toISO8601(LocalDateTime.now())
+        );
+
+        logger.error("Error thrown with status code: " + payload.getHttpStatusCode());
+
+        return new ResponseEntity<>(
+                payload,
+                payload.getHttpStatus()
+        );
+    }
+
+    @ExceptionHandler(value = DBEmptyException.class)
+    public ResponseEntity<Object> handleDBEmptyException(DBEmptyException e) {
+
+        //Returned to the client
+        ApiException payload = new ApiException(
+                e.getMessage(),
+                e.getCause(),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                DateUtil.toISO8601(LocalDateTime.now())
+        );
+
+        logger.error("Error thrown with status code: " + payload.getHttpStatusCode());
+
+        return new ResponseEntity<>(
+                payload,
+                payload.getHttpStatus()
+        );
+    }
+
+    @ExceptionHandler(value = JsonNotFoundException.class)
+    public ResponseEntity<Object> handleJsonNotFoundException(JsonNotFoundException e) {
+
+        //Returned to the client
+        ApiException payload = new ApiException(
+                e.getMessage(),
+                e.getCause(),
+                HttpStatus.NOT_FOUND,
+                DateUtil.toISO8601(LocalDateTime.now())
+        );
+
+        logger.error("Error thrown with status code: " + payload.getHttpStatusCode());
+
+        return new ResponseEntity<>(
+                payload,
+                payload.getHttpStatus()
+        );
+    }
+
+    //TODO: add success status.ok
 }
